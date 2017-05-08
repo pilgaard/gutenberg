6f05922693fa3c8d9637e33a7dcf693c8c9e9f3e
pilgaard <emil.pilgaard@gmail.com>
2017-05-08 09:31:52 +0200

    .

--- script.sql
+++ script.sql
@@ -16,48 +16,25 @@ primary key (id)
 drop table if exists cities;
 create table cities
 (
-geonameId		int,
-`name` 			varchar(200),
-asciiname		varchar(200),
-alternatenames	varchar(10000),
-lat				Decimal(10,8),
-`long`			Decimal(11,8),
-`feature class`	char(1),
-`feature code`	varchar(10),
-`country code`		char(2),
-cc2				varchar(200),
-`admin1 code`	varchar(20),
-`admin2 code`	varchar(80), 
-`admin3 code`	varchar(20),
-`admin4 code`	varchar(20),			
-population		bigint,
-elevation		int,
-dem				int,
-timezone		varchar(40),
+geonameId			int,
+`name` 				varchar(200),
+asciiname			varchar(200),
+alternatenames		varchar(10000),
+lat					Decimal(10,8),
+`long`				Decimal(11,8),
+`country code`		char(2),			
+population			bigint,
+elevation			int,
+dem					int,
+timezone			varchar(40),
 `modification date` date,
 primary key (geonameId)
 );
 
-drop table if exists continentCodes;
-create table continentCodes
-(
-geonameId	int,
-continent	varchar(20),
-`code`		varchar(50),
+LOAD DATA LOCAL INFILE '/Users/Emil/examproject/gutenberg/dbSetup.csv' INTO TABLE cities
+fields terminated by ',' enclosed by '"'
+lines terminated by '\n'
+ignore 1 lines;
 
-primary key (geonameId)
-);
-
-INSERT INTO `continentCodes` ()
-VALUES
-	(6255146, 'Africa', 'AF'),
-	(6255147, 'Asia', 'AS'),
-	(6255148, 'Europe', 'EU'),
-	(6255149, 'North America', 'NA'),
-	(6255150, 'South America', 'SA'),
-	(6255151, 'Oceania', 'OC'),
-	(6255152, 'Antarctica', 'AN');
-	
-LOAD DATA LOCAL INFILE '/Users/Emil/examproject/gutenberg/cities5000.txt' INTO TABLE cities;
 
 
