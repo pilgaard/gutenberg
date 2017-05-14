c615a91584841ad22643c1caf7260710ee1f2c84
AndreasHeindorff <heindorff94@gmail.com>
2017-05-14 14:58:43 +0200

    Added IDBFacade.

--- src/main/java/Database/MySQLDBFacade.java
+++ src/main/java/Database/MySQLDBFacade.java
@@ -5,15 +5,50 @@
  */
 package Database;
 
+import Entity.Book;
+import Entity.City;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  *
  * @author Andreas
  */
-public class MySQLDBFacade {
+public class MySQLDBFacade implements IDBFacade {
     
     private MySQLConnector connector;
     
     public MySQLDBFacade(MySQLConnector con){
         this.connector = con;
     } 
+
+    @Override
+    public List<Book> GetBooksByCity(String cityName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<City> GetCitiesByBookTitle(String bookTitle) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HashMap<Long, Long> GetGeoLocationByCity(City city) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Book> GetBooksByAuthorName(String authorName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HashMap<Long, Long> GetGeoLocationByBook(Book book) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Book> GetBooksByGeoLocation(Long latitude, Long longitude) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
