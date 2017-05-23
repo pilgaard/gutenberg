6cac2613a169adacf897f159e8058966bed18b84
AndreasHeindorff <heindorff94@gmail.com>
2017-05-23 18:45:42 +0200

    Gson change.

--- src/main/java/API/GraphWebController.java
+++ src/main/java/API/GraphWebController.java
@@ -7,7 +7,6 @@ package API;
 
 import Database.GraphFacade;
 import Util.JsonBuilder;
-import com.google.gson.Gson;
 import java.math.BigDecimal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class GraphWebController implements IWebController {
     
     private static GraphFacade graphFacade; 
-    private Gson gson = new Gson();
     
     public static void SetFacade(GraphFacade graphFacade){
         GraphWebController.graphFacade = graphFacade;
@@ -32,35 +30,35 @@ public class GraphWebController implements IWebController {
     @Override
     @RequestMapping(value="/cities", method=RequestMethod.GET, produces = "application/json")
     public @ResponseBody String GetCities( ) {
-        String json = gson.toJson(graphFacade.GetCities());
+        String json = JsonBuilder.gson.toJson(graphFacade.GetCities());
     return json;
 }
 
     @Override
     @RequestMapping(value="/booksbycity", method=RequestMethod.POST, produces = "application/json")
     public @ResponseBody String GetBooksByCity(@RequestParam("city") String City) {
-        String json = gson.toJson(graphFacade.GetBooksByCity(City));
+        String json = JsonBuilder.gson.toJson(graphFacade.GetBooksByCity(City));
     return json;
     }
 
     @Override
     @RequestMapping(value="/citiesbytitle", method=RequestMethod.POST, produces = "application/json")
     public @ResponseBody String GetCitiesByBookTitle(@RequestParam("title")String title) {
-        String json = gson.toJson(graphFacade.GetCitiesByBookTitle(title));
+        String json = JsonBuilder.gson.toJson(graphFacade.GetCitiesByBookTitle(title));
     return json;
     }
 
     @Override
     @RequestMapping(value="/booksbyauthor", method=RequestMethod.POST, produces = "application/json")
     public @ResponseBody String GetBooksByAuthorName(@RequestParam("authorName")String authorName) {
-        String json = gson.toJson(graphFacade.GetBooksByAuthorName(authorName));
+        String json = JsonBuilder.gson.toJson(graphFacade.GetBooksByAuthorName(authorName));
     return json;
     }
 
     @Override
     @RequestMapping(value="/booksbygeolacation", method=RequestMethod.POST, produces = "application/json")
     public @ResponseBody String GetBooksByGeoLocation(@RequestParam("latitude")BigDecimal latitude, @RequestParam("longitude")BigDecimal longitude) {
-        String json = gson.toJson(graphFacade.GetBooksByGeoLocation(latitude, longitude));
+        String json = JsonBuilder.gson.toJson(graphFacade.GetBooksByGeoLocation(latitude, longitude));
     return json;
     }
 
