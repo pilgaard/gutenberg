ce3839f3ffc0fbcacea80e1e3efa562b6e1c529d
pilgaard <emil.pilgaard@gmail.com>
2017-05-26 20:27:39 +0200

    .

--- src/main/java/api/Application.java
+++ src/main/java/api/Application.java
@@ -9,6 +9,10 @@ import database.GraphConnector;
 import database.GraphFacade;
 import database.MySQLConnector;
 import database.MySQLDBFacade;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -25,13 +29,24 @@ public class Application {
         start(args);
     }
     
-    public static void start(String[] args){
-        MySQLConnector mysqlCon = new MySQLConnector();
-        GraphConnector graphCon = new GraphConnector();
-        MySQLDBFacade mysqlFacade = new MySQLDBFacade(mysqlCon);
-        GraphFacade graphFacade = new GraphFacade(graphCon);
-        GraphWebController.SetFacade(graphFacade);
-        MySQLWebController.SetFacade(mysqlFacade);
-        SpringApplication.run(Application.class, args);
+    public static void start(String[] args) {
+        try {
+            InetAddress ip;
+            String hostname;
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            System.out.println("Your current IP address : " + ip);
+            System.out.println("Your current Hostname : " + hostname);
+            
+            MySQLConnector mysqlCon = new MySQLConnector();
+            GraphConnector graphCon = new GraphConnector();
+            MySQLDBFacade mysqlFacade = new MySQLDBFacade(mysqlCon);
+            GraphFacade graphFacade = new GraphFacade(graphCon);
+            GraphWebController.SetFacade(graphFacade);
+            MySQLWebController.SetFacade(mysqlFacade);
+            SpringApplication.run(Application.class, args);
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
